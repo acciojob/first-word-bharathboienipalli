5d0b1c9997dbc95a25430b3a7210ee735cd80555
bharathboienipalli <bharathboienipalli@gmail.com>
2025-05-20 08:32:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function firstWord(s) {
-  // your code here
+  s = s.trim();
+  const spaceIndex = s.indexOf(' ');
+  if (spaceIndex === -1) return s;
+  return s.substring(0, spaceIndex);
 }
-
-// Do not change the code below
 
 const s = prompt("Enter String:");
 alert(firstWord(s));
